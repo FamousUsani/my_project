cc661a7d3310dfda22cb44a4a5b55091539ec182
famous Usani <famousabove36@gmail.com>
2023-05-26 14:52:21 +0100

    adding html tags

--- php_output_string.php
+++ php_output_string.php
@@ -1,9 +1,9 @@
 <?php
-$name = "ETOWA USANI ARIKPO";
-$regno = "2021/HND/34382/CS";
-$soo = "CROSS RIVER STATE";
-$project = "DESIGN AND IMPLEMENTATION OF A WHATSAPP 
-BASED STUDENT ASSITANT CHATBOT";
+$name = "<h1>ETOWA USANI ARIKPO</h1>";
+$regno = "2021/HND/34382/CS<br>";
+$soo = "CROSS RIVER STATE<br>";
+$project = "<strong>DESIGN AND IMPLEMENTATION OF A WHATSAPP 
+BASED STUDENT ASSITANT CHATBOT</strong>";
 echo $name;
 echo $regno;
 echo $soo;
