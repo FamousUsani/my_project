5419a828bd4bd3a5569e48cd08de80c5ae7096d7
famous Usani <famousabove36@gmail.com>
2023-05-26 14:38:35 +0100

    using variables to output strings

---
+++ php_output_string.php
@@ -0,0 +1,12 @@
+<?php
+$name = "ETOWA USANI ARIKPO";
+$regno = "2021/HND/34382/CS";
+$soo = "CROSS RIVER STATE";
+$project = "DESIGN AND IMPLEMENTATION OF A WHATSAPP 
+BASED STUDENT ASSITANT CHATBOT";
+echo $name;
+echo $regno;
+echo $soo;
+echo $project;
+
+?>
